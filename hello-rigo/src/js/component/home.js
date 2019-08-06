e235644a5dfc7e848f1704a585073a3bb387848c
Josancev <josemanuel777@hotmail.com>
2019-08-06 22:29:07 +0000

    v7

--- hello-rigo/src/js/component/home.js
+++ hello-rigo/src/js/component/home.js
@@ -1,52 +1,15 @@
-import React from "react";
-
-//include images into your bundle
-
-//create your first component
-export class Traffic extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			SelectedLight: null
-		};
-	}
-
-	render() {
-		//console.log(this.state);
-		let redExtraClass = "";
-		if (this.state.SelectedLight == "red") redExtraClass = "selected";
-		let yellowExtraClass = "";
-		if (this.state.SelectedLight == "yellow") yellowExtraClass = "selected";
-		let greenExtraClass = "";
-		if (this.state.SelectedLight == "green") greenExtraClass = "selected";
-		return (
-			<div>
-				<div id="trafficTop" />
-				<div id="container">
-					<div
-						className={"red light " + redExtraClass}
-						onClick={() => this.setState({ SelectedLight: "red" })}
-					/>
-					<div
-						className={"yellow light " + yellowExtraClass}
-						onClick={() =>
-							this.setState({ SelectedLight: "yellow" })
-						}
-					/>
-					<div
-						className={"green light " + greenExtraClass}
-						onClick={() =>
-							this.setState({ SelectedLight: "green" })
-						}
-					/>
-				</div>
-				<div className="pt-5">
-					<h3>
-						<p>On click change the</p>
-						<p>traffic light colors!</p>
-					</h3>
-				</div>
-			</div>
-		);
-	}
+import React, { Fragment } from "react";
+import { Navbar } from "./navbar";
+import { Header } from "./header";
+import { Cards } from "./cards";
+import { Footer } from "./footer";
+export function Home(props) {
+	return (
+		<Fragment>
+			<Navbar />
+			<Header />
+			<Cards />
+			<Footer />
+		</Fragment>
+	);
 }
