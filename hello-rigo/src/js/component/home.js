0bc075ce583f66928b7fa57644bf3179135a350f
Josancev <josemanuel777@hotmail.com>
2019-08-06 23:03:06 +0000

    vr

--- hello-rigo/src/js/component/home.js
+++ hello-rigo/src/js/component/home.js
@@ -1,15 +1,28 @@
-import React, { Fragment } from "react";
-import { Navbar } from "./navbar";
-import { Header } from "./header";
-import { Cards } from "./cards";
-import { Footer } from "./footer";
-export function Home(props) {
-	return (
-		<Fragment>
-			<Navbar />
-			<Header />
-			<Cards />
-			<Footer />
-		</Fragment>
-	);
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class Landing extends Component {
+	render() {
+		return (
+			<div>
+				<h1>landing page</h1>
+			</div>
+		);
+	}
 }
+export default Landing;
+
+// import { Navbar } from "./navbar";
+// import { Header } from "./header";
+// import { Cards } from "./cards";
+// import { Footer } from "./footer";
+// export function Home(props) {
+// 	return (
+// 		<Fragment>
+// 			<Navbar />
+// 			<Header />
+// 			<Cards />
+// 			<Footer />
+// 		</Fragment>
+// 	);
+// }
